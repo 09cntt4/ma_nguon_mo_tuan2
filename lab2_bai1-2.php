900e60688a7bb9bd0dfefa88eda57e99c30c4451
09cntt4 <125049829+09cntt4@users.noreply.github.com>
2023-02-24 09:01:46 +0700

    Add files via upload

---
+++ lab2_bai1-2.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bai 1-2</title>
+</head>
+<style>
+    .display-box {
+        font-family: 'Orbitron', sans-serif;
+        background-color: #dcdbe1;
+        color: black;
+        width: 380px;
+        height: 100px;
+        border: 2px solid black;
+        border-collapse: collapse;
+    }
+    table{
+        margin-left: auto;
+        margin-right: auto;
+        text-align: center;
+
+    }
+    h1{
+        text-align: center;
+    }
+    input{
+        width: auto;
+    }
+    .box-2{
+        font-family: 'Orbitron', sans-serif;
+        background: #3b759e;
+        color: black;
+        border: 2px solid rgb(17, 213, 27);
+        border-collapse: collapse;
+        width: 720px;
+        height: 250px;
+    }
+</style>
+<body>
+    
+    <tr>
+        <h1>TÌM HIỂU KỸ THUẬT DOM TRONG JAVASCRIPT <br>
+            (DOM : Document Object Model)</h1>
+    </tr>
+    <table class="display-box" border="1">
+    <tr>
+        <td>Số dòng</td>
+        <td> <input type="text"></td><br>
+    </tr>
+    <tr>
+        <td>Số cột</td>
+        <td> <input type="text"></td>
+    </tr>
+        <td colspan="2">
+            <input type="button" value="Hiển thị">
+            <input type="button" value="Xóa">
+        </td>
+    </table>
+    <br>
+    <table class="box-2" border="1">
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+</body>
+</html>
